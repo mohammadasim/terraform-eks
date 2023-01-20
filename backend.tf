81f12c84f22ecce40c74a3eb8db6a3c19394cadd
Mohammad Asim Ayub <mohammad.ayub@gmail.com>
2023-01-20 07:30:39 +0000

    removed file

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "<account-number>terraform-state-bucket"
-    key     = "eks-deployment/terraform.tfstate"
-    region  = "eu-west-1"
-    profile = "default"
-  }
-}
