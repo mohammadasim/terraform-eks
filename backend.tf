a942770c69ff8aaab2449928dda34da7d50ccb8a
Mohammad Asim Ayub <mohammad.ayub@gmail.com>
2023-01-20 07:16:50 +0000

    updated bucket

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "191998317647terraform-state-bucket"
+    bucket  = "<account-number>terraform-state-bucket"
     key     = "eks-deployment/terraform.tfstate"
     region  = "eu-west-1"
     profile = "default"
